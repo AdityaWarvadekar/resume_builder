83811fa4237f790233b28172eeafaf2c3b2de448
AdityaWarvadekar <adityawarvadekar11@gmail.com>
2024-01-25 11:18:45 +0530

    Create PDF Template and Start the GUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,161 @@
 import logo from './logo.svg';
 import './App.css';
+import PDFDoc from './components/PDFDoc';
+import { PDFViewer } from '@react-pdf/renderer';
+import { useState } from 'react';
 
 function App() {
+  const secions = [
+    {
+      secHeading: "EXPERIENCE",
+      headings: [
+        {
+          heading: "Newbieron Technologies",
+          subHeading: "Web Developer Intern",
+          date: "Jul 2023 - Sep 2023",
+          place: "New Delhi",
+          list: [
+            "Created Responsive Pages", "Attended this this this event cerononsdgek",
+            "Contributed this this this and thids",
+            "Got this award and that appraisal"
+          ]
+        },
+        {
+          heading: "Newbieron Technologies",
+          subHeading: "Web Developer Intern",
+          date: "Jul 2023 - Sep 2023",
+          place: "New Delhi",
+          list: [
+            "Created Responsive Pages", "Attended this this this event cerononsdgek",
+            "Contributed this this this and thids",
+            "Got this award and that appraisal"
+          ],
+        }
+      ]
+    },
+    {
+      secHeading: "EDUCATION",
+      headings: [
+        {
+          heading: "Guru Gobind Singh Indraprastha University",
+          subHeading: "Bachelors of Technology in Computer Science",
+          date: "Jul 2025",
+          place: "Dwarka, Delhi"
+        }
+      ]
+    },
+    {
+      secHeading: "PROJECTS",
+      headings: [
+
+        {
+          heading: "TnP_USICT",
+          side: "React, MongoDB, MERN, AWS",
+          projectLink: "www.google.com",
+          list: [
+            "Developed Full Stack Application",
+            "Implemented Authentication using JWT Tokens",
+          ],
+          date: "Aug 23 - Sep 23"
+        }
+      ]
+    },
+    {
+      secHeading: "TECHNICAL SKILLS",
+      headings: [
+        {
+          para: [
+            {
+              title: "Languages",
+              description: "C++, C, Java, Python, HTML, CSS, JS, MySQL"
+            },
+            {
+              title: "Frameworks",
+              description: "C++, C, Java, Python, HTML, CSS, JS, MySQL"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      secHeading: "ACHIEVEMENTS",
+      headings: [
+        {
+          para: [
+            {
+              description: "Secured First Position in IEEE Synapse Hackathon"
+            },
+            {
+              description: "Secured Second Position in IEEE Hack-U-Thon"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+
+  const [input, setInput] = useState({
+    fname: "",
+    lname: "",
+    address: "",
+    phone: "",
+    email: "",
+    linkedin: "",
+    github: ""
+  })
+
+  const onChangeInput = (e) => { setInput({ ...input, [e.target.name]: e.target.value }) }
+
+  let data = [];
+  let basicInfo = data[0]? data[0]: "";
+  const captureInput = ()=>{
+    data.push(input);
+    console.log(data);
+  }
+
+  const [sections, setSections] = useState([]);
+
+
+
   return (
     <div className="App">
-      HELLO
+
+      <div className="container w-50 d-flex flex-column align-items-center">
+        <div className='d-flex'>
+          <div className='d-flex flex-column align-items-center px-3'>
+            <p className='my-2'>First Name:</p>
+            <input type="Text" className="form-control" id="" name="fname" value={input.fname} onChange={onChangeInput} />
+          </div>
+          <div className='d-flex flex-column align-items-center px-3'>
+            <p className='my-2'>Last Name:</p>
+            <input type="Text" className="form-control" id="" name="lname" value={input.lname} onChange={onChangeInput} />
+          </div>
+        </div>
+        <p className='my-2'>Address: </p>
+        <input type="Text" className="form-control" id="" name="address" value={input.address} onChange={onChangeInput} />
+        <div className='d-flex my-5'>
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <p>Phone: </p>
+            <input type="Text" className="form-control" id="" name="phone" value={input.phone} onChange={onChangeInput} />
+          </div>
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <p>Email: </p>
+            <input type="Text" className="form-control" id="" name="email" value={input.email} onChange={onChangeInput} />
+          </div>
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <p>Linkedin: </p>
+            <input type="Text" className="form-control" id="" name="linkedin" value={input.linkedin} onChange={onChangeInput} />
+          </div>
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <p>Github: </p>
+            <input type="Text" className="form-control" id="" name="github" value={input.github} onChange={onChangeInput} />
+          </div>
+        </div>
+        <button className="btn btn-success" onClick={captureInput}>Submit</button>
+
+      </div>
+
+      <PDFViewer width={"50%"} height={"700px"}><PDFDoc fname={basicInfo.fname} lname={basicInfo.lname} phone={basicInfo.phone} email={basicInfo.email} linkedin={basicInfo.linkedin} github={basicInfo.github} sections={sections} /></PDFViewer>
     </div>
   );
 }
---
+++ src/components/PDFDoc.js
@@ -0,0 +1,325 @@
+import { Document, Page, StyleSheet, Text, View, Image, Font, Link } from '@react-pdf/renderer'
+import React from 'react'
+import Regular from "./fonts/cmunrm.ttf"
+import Bold from "./fonts/cmunbx.ttf"
+import BoldItalic from "./fonts/cmunbi.ttf"
+import Italic from "./fonts/cmunti.ttf"
+import phone from "./img/phone.jpg"
+import email from "./img/email.jpg"
+import linkedin from "./img/linkedin.jpg"
+import github from "./img/github.jpg"
+
+
+function PDFDoc(props) {
+    // Font.register({
+    //  family: 'CormorantGaramond_bold', 
+    //  format: "truetype",
+    //  src: CormorantGaramond_bold, 
+    //  fontStyle: 'normal', 
+    //  fontWeight: 'normal', 
+    // },
+    // );
+    // Font.register({
+    //     family: 'CormorantGaramond_medium', 
+    //     format: "truetype",
+    // src: CormorantGaramond_medium, 
+    //  fontStyle: 'normal', 
+    //  fontWeight: 'normal', 
+    //    });
+
+    Font.register({
+        family: 'Regular',
+        format: "truetype",
+        src: Regular,
+        //  fontStyle: 'normal', 
+        //  fontWeight: 'normal', 
+    });
+
+    Font.register({
+        family: 'Bold',
+        format: "truetype",
+        src: Bold,
+        //  fontStyle: 'normal', 
+        //  fontWeight: 'normal', 
+    });
+
+    Font.register({
+        family: 'BoldItalic',
+        format: "truetype",
+        src: BoldItalic,
+        //  fontStyle: 'normal', 
+        //  fontWeight: 'normal', 
+    });
+
+    Font.register({
+        family: 'Italic',
+        format: "truetype",
+        src: Italic,
+        //  fontStyle: 'normal', 
+        //  fontWeight: 'normal', 
+    });
+
+    const ListItem = ({ children }) => {
+        return (
+            <View style={styles.row}>
+                <View style={styles.bullet}>
+                    <Text>{'\u2022' + "   "}</Text>
+                </View>
+                <Text>{children}</Text>
+            </View>
+        )
+    }
+
+    const Section = (props) => {
+        let headings = props.headings ? props.headings : [];
+        return (
+            <>
+                <View style={styles.section}>
+                    <Text><Text style={styles.headingCaps}>{props.secHeading? props.secHeading.slice(0, 1) : ""}</Text>{props.secHeading? props.secHeading.slice(1) : ""}</Text>
+                </View>
+                {headings.map((ele) => {
+                    return (
+                        <>
+                            <View style={styles.headingLayout}>
+                                {(ele.side) ? <Text style={styles.heading}>{ele.heading} | <Text style={styles.subHeading}>{ele.side}</Text></Text> : <Text style={styles.heading}>{ele.heading}</Text>}
+                                <Text style={styles.date}>{ele.date}</Text>
+                            </View>
+                            <View style={styles.headingLayout}>
+                                <Text style={styles.subHeading}>{ele.subHeading}</Text>
+                                <Text style={styles.subHeading}>{ele.place}</Text>
+                            </View>
+                            <View style={styles.listLayout}>
+                                {ele.list ?
+                                    ele.list.map(element => {
+                                        return (
+                                            <ListItem>{element}</ListItem>
+                                        );
+                                    })
+                                    : ""
+                                };
+                                {(ele.projectLink ? <ListItem>Project Link- <Link src={ele.projectLink}>{ele.projectLink}</Link></ListItem> : "")}
+                            </View>
+                            <View style={styles.para}>
+                                {ele.para ? ele.para.map((td) => {
+                                    return (<View style={{ display: "flex", flexDirection: "row" }}>{td.title ? <Text style={styles.title}>{td.title} : </Text> : ""}<Text style={styles.desc}>{td.description ? td.description : ""}</Text></View>)
+                                }) : ""}
+                            </View>
+                        </>
+                    );
+                })}
+
+            </>
+        );
+    }
+
+    const styles = StyleSheet.create({
+        page: {
+            // flexDirection: "column",
+            padding: "15px 30px",
+            fontFamily: "Regular",
+            justifyContent: "space-around"
+        },
+        header: {
+            flexGrow: 1,
+            textAlign: "center",
+            fontSize: "22.5px",
+            fontFamily: "Regular"
+        },
+        subHeader: {
+            textAlign: "center",
+            fontSize: "11px",
+            // fontFamily: "Times-Roman"
+        },
+        caps: {
+            fontSize: "29.5px"
+        },
+        icon: {
+            height: "8px",
+            width: "8px",
+        },
+        inline: {
+            flexDirection: "row",
+            flexWrap: "wrap",
+            textAlign: "center",
+            fontSize: "10px",
+            justifyContent: "center",
+            // padding: " 0 5%",
+            // width: "80%"
+        },
+        section: {
+            flexDirection: "row",
+            margin: "10px 0 2px 0",
+            fontSize: "9px",
+            borderBottom: "1px solid black"
+        },
+        headingLayout: {
+            flexDirection: "row",
+            justifyContent: "space-between",
+            padding: "0 12px"
+        },
+        heading: {
+            fontSize: "11px",
+            fontFamily: "Bold",
+            // fontWeight:"bold",
+            padding: 0,
+            marginTop: "1px"
+        },
+        headingCaps: {
+            fontSize: "12px"
+        },
+        date: {
+            fontFamily: "Regular",
+            fontSize: "11px",
+            marginTop: "1px"
+        },
+        subHeading: {
+            fontSize: "10px",
+            fontFamily: "Italic"
+        },
+
+        //LIST PART
+        listLayout: {
+            padding: "0 20px"
+        },
+        row: {
+            display: 'flex',
+            flexDirection: 'row',
+            fontSize: "10px"
+        },
+        bullet: {
+            fontSize: "8px",
+            height: "100%"
+        },
+
+        //DESCRIPTIONS
+
+        para: {
+            padding: "0 10px",
+            textAlign: 'left'
+        },
+        title: {
+            fontSize: "10px",
+            fontFamily: "Bold"
+        },
+        desc: {
+            fontSize: "10px",
+            fontFamily: "Regular"
+        }
+    });
+    return (
+        <Document>
+            <Page size={"LETTER"} style={styles.page}>
+                <View style={styles.header}>
+                    <Text><Text style={styles.caps}>{props.fname? props.fname.slice(0, 1):""}</Text>{props.fname? props.fname.slice(1):""} <Text style={styles.caps}>{props.lname?props.lname.slice(0, 1):""}</Text>{props.lname?props.lname.slice(1):""}</Text>
+                    <Text style={styles.subHeader}>{props.address ? props.address : ""}</Text>
+                    <View style={styles.inline}>
+                        <Text style={{ padding: "0 5px" }}><Image src={props.phone? phone: ""} style={styles.icon} /> {props.phone ? props.phone : ""}</Text>
+                        <Text style={{ textDecoration: "underline", padding: "0 5px" }}><Image src={props.email? email:""} style={styles.icon} /><Link src={props.email ? props.email : ""} /> {props.email ? props.email : ""}</Text>
+                        <Text style={{ textDecoration: "underline", padding: "0 5px" }}><Image src={props.linkedin ? linkedin : ""} style={styles.icon} /><Link src={props.linkedin ? props.linkedin : ""} /> {props.linkedin ? props.linkedin : ""}</Text>
+                        <Text style={{ textDecoration: "underline", padding: "0 5px" }}><Image src={props.github ? github : ""} style={styles.icon} /><Link src={props.github ? props.github : ""} /> {props.github ? props.github : ""}</Text>
+                    </View>
+
+                    {props.sections ?
+                        props.sections.map((section) => {
+                            return (
+                                <>
+                                    <Section secHeading={section.secHeading} headings={section.headings}/>
+                                </>
+                            )
+                        })
+                        : ""}
+
+                    {/* EXPERIENCE */}
+
+
+{/* 
+                    <Section secHeading="EXPERIENCE" headings={[{
+                        heading: "Newbieron Technologies",
+                        date: "Jul 2023 - Sep 2023",
+                        subHeading: "Web Development Intern",
+                        place: "New Delhi, Delhi",
+                        list: ["Created Responsive Pages", "Attended this this this event cerononsdgek", "Contributed this this this and thids", "Got this award and that appraisal"],
+                    },
+                    {
+                        heading: "Newbieron Technologies",
+                        date: "Jul 2023 - Sep 2023",
+                        subHeading: "Web Development Intern",
+                        place: "New Delhi, Delhi",
+                        list: ["Created Responsive Pages", "Attended this this this event cerononsdgek", "Contributed this this this and thids", "Got this award and that appraisal"],
+                        projectLink: "https://www.google.com",
+                        side: "React, MongoDB, Express, Node, AWS"
+                    }]
+                    } />
+
+                    <Section secHeading="EDUCATION" headings={[{
+                        heading: "Guru Gobind Singh Indraprastha University",
+                        date: "Jul 2025",
+                        subHeading: "Bachelor of Technology in Computer Science",
+                        place: "Dwarka, Delhi",
+
+                    }]
+                    } />
+
+
+                    <Section secHeading="EXPERIENCE" headings={[{
+
+                        para: [
+                            {
+                                title: "Cogitans, Techspace",
+                                description: "Web Development Lead, mentored students"
+                            },
+                            {
+
+                                description: "Web Development Lead, mentored students"
+                            },
+                            {
+                                title: "Cogitans, Techspace",
+
+                            }
+                        ]
+                    },
+
+                    ]
+                    } />
+
+
+                    <Section secHeading="EXPERIENCE" headings={[{
+                        heading: "Newbieron Technologies",
+                        date: "Jul 2023 - Sep 2023",
+                        subHeading: "Web Development Intern",
+                        place: "New Delhi, Delhi",
+                        list: ["Created Responsive Pages", "Attended this this this event cerononsdgek", "Contributed this this this and thids", "Got this award and that appraisal"],
+                    },
+                    {
+                        heading: "Newbieron Technologies",
+                        date: "Jul 2023 - Sep 2023",
+                        subHeading: "Web Development Intern",
+                        place: "New Delhi, Delhi",
+                        list: ["Created Responsive Pages", "Attended this this this event cerononsdgek", "Contributed this this this and thids", "Got this award and that appraisal"],
+                    }]
+                    } /> */}
+
+
+                </View>
+
+
+
+
+
+            </Page>
+        </Document>
+    );
+
+
+
+
+}
+
+
+
+export default PDFDoc
+
+
+
+
+// https://github.com/diegomura/react-pdf/issues/1075
